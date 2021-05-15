e841fca27187a6c61008458ed53c743c78c1453c
Johann <johann.de-almeida@lacatholille.fr>
2021-05-15 18:54:58 +0200

    :sparkles: Ajout Micro Service Post

---
+++ post/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO post (text, title, author)
+VALUES ('Bienvenue', 'Ceci est un message de bienvenue !',  1);
+
+INSERT INTO post (text, title, shared, author)
+VALUES ('hAPI friends', 'hAPI friends est une API réseau social faite avec Spring !', 0, 1);
